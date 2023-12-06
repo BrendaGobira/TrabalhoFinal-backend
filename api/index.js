19c923a8db44b8b900a76391b1cb8271da2216b0
Brenda Gobira <bgobiravalenca@unifei.edu.br>
2023-12-05 22:36:26 -0300

    reorganização dos arquivos

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const usuariosRouter = require("./usuarios");
-const backgroundsRouter = require("./backgrounds");
+const usuariosRouter = require("../usuarios");
+const backgroundsRouter = require("../backgrounds");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Ok – Servidor disponível.");
+});
 
 app.use(usuariosRouter);
 app.use(backgroundsRouter);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./api/index');
-const config = require("./api/config");
+const config = require("./config");
 
 app.listen(config.port, () =>
   console.log("Servidor funcionando na porta " + config.port)
